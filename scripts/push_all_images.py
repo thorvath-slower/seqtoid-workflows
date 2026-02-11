0104459580163f130a4cfcc439532a72f2d5af07
Joseph Sims <jsims@slower.ai>
2026-02-10 18:51:11 -0700

    Update ECR push script to push all Docker tags, and catch errors, correctly

--- scripts/push_all_images.py
+++ scripts/push_all_images.py
@@ -4,37 +4,82 @@ import subprocess
 import warnings
 
 workflow_versions = {
-    "amr": "v1.4.2",
-    "benchmark": "v0.0.3",
-    "bulk-download": "v0.0.9",
-    "consensus-genome": "v3.5.4",
-    # "consensus-genome": "v3.5.5",
-    "diamond": "v1.1.0",
-    # "diamond": "v1.1.1",
-    "host-genome-generation": "v0.2.0",
-    "index-generation": "v2.4.8",
-    "long-read-mngs": "v0.7.11",
-    "minimap2": "v1.0.0",
-    # "minimap2": "v1.0.1",
-    "phylotree-ng": "v6.11.0",
-    "short-read-mngs": "v8.3.11",
+    "amr": [
+        "v1.4.2"
+    ],
+    "benchmark": [
+        "v0.0.3"
+    ],
+    "bulk-download": [
+        "v0.0.9"
+    ],
+    "consensus-genome": [
+        "v3.5.1",
+        "v3.5.4",
+        "v3.5.5"
+    ],
+    "diamond": [
+        "v1.1.0",
+        "v1.1.1"
+    ],
+    "host-genome-generation": [
+        "v0.2.0"
+    ],
+    "index-generation": [
+        "v2.4.8"
+    ],
+    "long-read-mngs": [
+        "v0.7.11"
+    ],
+    "minimap2": [
+        "v1.0.0",
+        "v1.0.1"
+    ],
+    "phylotree-ng": [
+        "v6.11.0"
+    ],
+    "short-read-mngs": [
+        "v8.3.11"
+    ],
 }
 
-image_prefix = "491013321714.dkr.ecr.us-west-2.amazonaws.com"
 
-def run_cmd(cmd):
+def run_cmd(cmd, input=None):
     print(' '.join(cmd))
     try:
-        res = subprocess.run(cmd)
+        if input is None:
+            p = subprocess.run(cmd, encoding='utf-8', stdout=subprocess.PIPE)
+        else:
+            p = subprocess.run(cmd, input=input, encoding='utf-8', stdout=subprocess.PIPE)
+        if p.returncode != 0:
+            raise Exception("Command failed!")
+        return p.stdout.strip()
     except Exception as ex:
         warnings.warn(ex)
+        raise
+
+
+account = run_cmd(["aws", "sts", "get-caller-identity", "--query", "Account", "--output", "text"])
+docker_url = f"{account}.dkr.ecr.us-west-2.amazonaws.com"
+print(f"docker_url = {docker_url}")
+
+password = run_cmd(["aws", "ecr", "get-login-password", "--region", "us-west-2"])
+# print(f"password = {password}")
+
+res = run_cmd(["docker", "login", "--username", "AWS", "--password-stdin", docker_url], password)
+print(f"Docker Login: {res}")
+
 
 def main():
-    for key, value in workflow_versions.items():
-        #run_cmd(["docker", "buildx", "build", "--platform", "linux/amd64", "--build-context", "lib=lib", f"workflows/{key}", "--tag", f"{image_prefix}/{key}:latest"])
-        run_cmd(["docker", "tag", f"{image_prefix}/{key}:latest", f"{image_prefix}/{key}:{value}"])
-        run_cmd(["docker", "push", f"{image_prefix}/{key}:latest"])
-        run_cmd(["docker", "push", f"{image_prefix}/{key}:{value}"])
+    for module, versions in workflow_versions.items():
+        # --platform linux/amd64,linux/arm64
+        cmd = ["docker", "buildx", "build", "--platform", "linux/amd64", "--build-context", "lib=lib", f"workflows/{module}"]
+        cmd += ["--tag", f"{docker_url}/{module}:latest"]
+        for version in versions:
+            cmd += ["--tag", f"{docker_url}/{module}:{version}"]
+        cmd += ["--push"]
+        run_cmd(cmd)
+
 
 if __name__ == "__main__":
     main()
