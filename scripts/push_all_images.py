c40d5446de7aeb1273c4c8d939db1fd6db66b9a7
Joseph Sims <jsims@slower.ai>
2026-02-20 13:03:48 -0700

    Update ECR push script to build and deploy images based on an AWS profile, using argparse

--- scripts/push_all_images.py
+++ scripts/push_all_images.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import subprocess
 import warnings
 
@@ -44,6 +45,24 @@ workflow_versions = {
 }
 
 
+def docker_login(profile: str):
+    account = run_cmd(["aws", "sts", "get-caller-identity", "--profile", profile, "--query", "Account", "--output", "text"])
+    if not account:
+        raise Exception("AWS Profile Has no Account")
+    print(f"account = {account}")
+
+    password = run_cmd(["aws", "ecr", "get-login-password", "--region", "us-west-2", "--profile", profile])
+    print(f"password = {password}")
+
+    docker_url = f"{account}.dkr.ecr.us-west-2.amazonaws.com"
+    print(f"docker_url = {docker_url}")
+
+    res = run_cmd(["docker", "login", "--username", "AWS", "--password-stdin", docker_url], password)
+    print(f"Docker Login: {res}")
+
+    return docker_url
+
+
 def run_cmd(cmd, input=None):
     print(' '.join(cmd))
     try:
@@ -59,18 +78,18 @@ def run_cmd(cmd, input=None):
         raise
 
 
-account = run_cmd(["aws", "sts", "get-caller-identity", "--query", "Account", "--output", "text"])
-docker_url = f"{account}.dkr.ecr.us-west-2.amazonaws.com"
-print(f"docker_url = {docker_url}")
-
-password = run_cmd(["aws", "ecr", "get-login-password", "--region", "us-west-2"])
-# print(f"password = {password}")
-
-res = run_cmd(["docker", "login", "--username", "AWS", "--password-stdin", docker_url], password)
-print(f"Docker Login: {res}")
-
-
 def main():
+    parser = argparse.ArgumentParser(description="Create all Workflow Images and push them to ECR")
+    parser.add_argument("-p", "--profile", type=str, help="AWS Profile", required=True)
+    # parser.add_argument( "-v", "--verbose", help="Enable verbose output", action="store_true" )
+    args = parser.parse_args()
+
+    profile = args.profile
+    if not profile:
+        raise Exception("AWS Profile Required")
+
+    docker_url = docker_login(args.profile)
+
     for module, versions in workflow_versions.items():
         # --platform linux/amd64,linux/arm64
         cmd = ["docker", "buildx", "build", "--platform", "linux/amd64", "--build-context", "lib=lib", f"workflows/{module}"]
