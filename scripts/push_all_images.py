476e17b545078039c0e55de7988effd628913ad7
Joseph Sims <jsims@slower.ai>
2026-03-20 12:22:41 -0600

    push_all_images.py no longer pre-loads, but just sets credentials as docker secrets, and lets the Dockerfile manage it's own files.

--- scripts/push_all_images.py
+++ scripts/push_all_images.py
@@ -1,84 +1,50 @@
 #!/usr/bin/env python3
 
 import argparse
-import boto3
-import os
-from pathlib import Path
 import subprocess
+import tempfile
 
-workflow_dict = {
-    "amr": {
-        "versions": [
-            "v1.4.2"
-        ],
-        "files": {
-            "card.json": {
-                "bucket": "seqtoid-public-references",
-                "blob": "card/2023-05-22/card.json"
-            }
-        }
-    },
-    "benchmark": {
-        "versions": [
-            "v0.0.3"
-        ]
-    },
-    "bulk-download": {
-        "versions": [
-            "v0.0.9"
-        ]
-    },
-    "consensus-genome": {
-        "versions": [
-            "v3.5.1",
-            "v3.5.4",
-            "v3.5.5"
-        ]
-    },
-    "diamond": {
-        "versions": [
-            "v1.1.0",
-            "v1.1.1"
-        ]
-    },
-    "host-genome-generation": {
-        "versions": [
-            "v0.2.0"
-        ]
-    },
-    "index-generation": {
-        "versions": [
-            "v2.4.8"
-        ]
-    },
-    # "legacy-host-filter": {},
-    "long-read-mngs": {
-        "versions": [
-            "v0.7.11"
-        ]
-    },
-    "minimap2": {
-        "versions": [
-            "v1.0.0",
-            "v1.0.1"
-        ]
-    },
-    "phylotree-ng": {
-        "versions": [
-            "v6.11.0"
-        ]
-    },
-    "short-read-mngs": {
-        "versions": [
-            "v8.3.11"
-        ],
-        "files": {
-            "HISAT2.zip": {
-                "bucket": "seqtoid-public-references",
-                "blob": "test/hisat2/hisat2.zip"
-            }
-        }
-    }
+s3_database_bucket = "seqtoid-public-references"
+
+workflow_versions = {
+    "amr": [
+        "v1.4.2"
+    ],
+    "benchmark": [
+        "v0.0.3"
+    ],
+    "bulk-download": [
+        "v0.0.9"
+    ],
+    "consensus-genome": [
+        "v3.5.1",
+        "v3.5.4",
+        "v3.5.5"
+    ],
+    "diamond": [
+        "v1.1.0",
+        "v1.1.1"
+    ],
+    "host-genome-generation": [
+        "v0.2.0"
+    ],
+    "index-generation": [
+        "v2.4.8"
+    ],
+    # "legacy-host-filter": [ ],
+    "long-read-mngs": [
+        "v0.7.11"
+    ],
+    "minimap2": [
+        "v1.0.0",
+        "v1.0.1"
+    ],
+    "phylotree-ng": [
+        "v6.11.0"
+    ],
+    "short-read-mngs": [
+        "v8.3.11"
+    ],
 }
 
 
@@ -114,29 +80,11 @@ def docker_login(profile: str):
     return docker_url
 
 
-def download_from_s3(s3_client, bucket: str, key: str, filename: str):
-    if not Path(filename).is_file():
-        print(f"download_from_s3 downloading blob [{key}] => [{filename}]")
-        s3_client.download_file(bucket, key, filename)
-    else:
-        print(f"download_from_s3: already exists [{filename}]")
-
-
-def download_files_from_s3(s3_client, local_directory: str, files_config_dict: dict[str, dict]):
-    print("local_directory = ", local_directory)
-    # print("files_config_dict =", files_config_dict)
-    if files_config_dict:
-        os.makedirs(local_directory, exist_ok=True)
-        for filename, blob_config_dict in files_config_dict.items():
-            # print("filename =", filename)
-            # print("blob_config_dict =", blob_config_dict)
-            bucket = blob_config_dict["bucket"]
-            # print("bucket =", bucket)
-            blob = blob_config_dict["blob"]
-            # print("blob =", blob)
-            download_from_s3(s3_client, bucket, blob, f"{local_directory}/{filename}")
-    else:
-        print("No files to download from S3")
+def write_aws_credentials(profile: str, file):
+    aws_credentials = run_cmd(["aws", "configure", "export-credentials", "--profile", profile, "--format", "env"])
+    # print("aws_credentials =", aws_credentials)
+    file.write(aws_credentials)
+    file.seek(0)
 
 
 def main():
@@ -149,36 +97,35 @@ def main():
         raise Exception("AWS Profile Required")
     print("aws_profile =", aws_profile)
 
+    build_args_dict = {
+        "S3_DATABASE_BUCKET": s3_database_bucket
+    }
+    print(f"build_args_dict =", build_args_dict)
+
     env_dict = {
-        # "AWS_CREDENTIALS": aws_credentials,
         "DOCKER_BUILDKIT": "1"
     }
-    # print(f"env_dict =", env_dict)
-
-    aws_session = boto3.Session(profile_name=aws_profile)
-    s3_client = aws_session.client('s3')
+    print(f"env_dict =", env_dict)
 
     docker_url = docker_login(aws_profile)
 
-    for workflow_name, workflow_config_dict in workflow_dict.items():
-        print(f"Creating Workflow [{workflow_name}]")
-        # print("workflow_config =", workflow_config_dict)
-        # Download S3 blobs into the workflows/<workflow_name>/tmp/ directory
-        download_files_from_s3(
-            s3_client,
-            f"workflows/{workflow_name}/tmp",
-            workflow_config_dict.get("files") or {}
-        )
+    with tempfile.NamedTemporaryFile(mode='w+', delete=True) as credentials_file:
+        print(f"Temporary AWS Credentials file created at: {credentials_file.name}")
 
-        # --platform linux/amd64,linux/arm64 --progress=plain #TODO:
-        cmd = ["docker", "buildx", "build", "--platform", "linux/amd64", "--progress=plain", "--build-context", "lib=lib", f"workflows/{workflow_name}"]
-        cmd += ["--tag", f"{docker_url}/{workflow_name}:latest"]
-        versions = workflow_config_dict["versions"]
-        # print("versions =", versions)
-        for version in versions:
-            cmd += ["--tag", f"{docker_url}/{workflow_name}:{version}"]
-        cmd += ["--push"]
-        run_cmd(cmd, env=env_dict)
+        write_aws_credentials(aws_profile, credentials_file)
+
+        for workflow_name, versions in workflow_versions.items():
+            print(f"Creating Workflow [{workflow_name}]")
+            # --platform linux/amd64,linux/arm64 --progress=plain #TODO:
+            cmd = ["docker", "buildx", "build", "--platform", "linux/amd64", "--progress=plain", "--build-context", "lib=lib", f"workflows/{workflow_name}"]
+            cmd += ["--tag", f"{docker_url}/{workflow_name}:latest"]
+            cmd += ["--secret", f"id=aws,src={credentials_file.name}"]
+            for k, v in build_args_dict.items():
+                cmd += ["--build-arg", f"{k}={v}"]
+            for version in versions:
+                cmd += ["--tag", f"{docker_url}/{workflow_name}:{version}"]
+            cmd += ["--push"]
+            run_cmd(cmd, env=env_dict)
 
     print(f"Done")
 
